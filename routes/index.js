6ee6f6a8324297df5d73a1b10cd16bcc24e76800
ALAJAKA <tkvl1017@gmail.com>
2023-02-01 23:51:08 +0900

    기본 세팅중

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/',(req,res)=>{
-  res.send("hello World");
+  res.render("login");
 });
 
 module.exports = router;
+
+
+// 깃허브 - 내피시 폴더 - 작업폴더
